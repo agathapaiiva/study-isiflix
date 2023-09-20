453df6cdec5dc36ec741114e56b8ce15fb3b480e
Agatha Paiva <agathahost@gmail.com>
2023-09-19 23:20:53 -0300

    commit initial

---
+++ case-isiBank/src/main/java/br/com/isiflix/isibank/IsibankApplication.java
@@ -0,0 +1,13 @@
+package br.com.isiflix.isibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsibankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsibankApplication.class, args);
+	}
+
+}
---
+++ case-isiBank/src/test/java/br/com/isiflix/isibank/IsibankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isiflix.isibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
